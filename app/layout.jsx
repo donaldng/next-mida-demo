47a30f2064fd09125c071120b70022bc0dc76b6c
Donald Ng <donaldyann@gmail.com>
2025-03-24 21:23:25 +0800

    Added mida script

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
         <html lang="en" data-theme="lofi">
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
+                <script type="text/javascript" async src="https://cdn.mida.so/js/optimize.js?key=oA05VeLkBJEMbREMq3X2Kv"></script>
             </head>
             <body className="antialiased text-white bg-blue-900">
                 <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
