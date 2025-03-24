eb2b3d013dd86a21c6bff554d375298826423c37
Donald Ng <donaldyann@gmail.com>
2025-03-24 22:26:46 +0800

    Added new param

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,7 +15,11 @@ export default function RootLayout({ children }) {
         <html lang="en" data-theme="lofi">
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
-                <MidaScript projectKey="oA05VeLkBJEMbREMq3X2Kv" useAntiFlicker={true} />
+                <MidaScript projectKey="oA05VeLkBJEMbREMq3X2Kv" 
+                    useAntiFlicker={true}
+                    isSPA={true}
+                    sync={true}
+                />
             </head>
             <body className="antialiased text-white bg-blue-900">
                 <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
