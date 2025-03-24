d37ac84ea98577015ad2d34284fe9a37dda90231
Donald Ng <donaldyann@gmail.com>
2025-03-24 21:37:16 +0800

    Import MidaScript

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import { Footer } from '../components/footer';
 import { Header } from '../components/header';
+import { MidaScript } from 'mida-nextjs';
 
 export const metadata = {
     title: {
